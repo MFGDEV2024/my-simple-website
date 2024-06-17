5eb78e8beda6ba27bdd804e6c2f5f6464036300a
MFGDEV2024 <matthew.f.gilbertson@gmail.com>
2024-06-16 22:43:32 -0400

    adds core structure and initial input functionality

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello, World!');
+    const button = document.getElementById('update-button')
+    const updatesentence = document.getElementById('updateable-sentence')
+    const inputSentence = document.getElementById('inputSentence')
+    const mathSelections = document.querySelectorAll('.mathSelections')
+    const operatorSelections = document.getElementById('math_Operator')
+    const storage = {};
+
+    // selection entry recording
+    mathSelections.forEach(mathSelections => {
+        mathSelections.addEventListener('input', (event) => {
+            const mathSelectionsID = event.target.id; //get specific math selection ID
+            const mathSelectionsValue = event.target.value; //get the value of the specifc math selectoin
+            storage[mathSelectionsID] = mathSelectionsValue; // store the value in the storage object
+            console.log(storage);
+        })
+    })
+
+    operatorSelections.addEventListener('input', (event) => {
+        const operatorSelectionValue = event.target.value
+        const operatorSelectionOutput = document.getElementById('MATHoperator')
+        operatorSelectionOutput.textContent = `${operatorSelectionValue}`
+    })
+
+    // data retrieval
+
+    // calculation
+    button.addEventListener('click', () => {
+        const inputValue = inputSentence.value;
+        updatesentence.textContent = `${inputValue}`
+    });
+
+   
 });
